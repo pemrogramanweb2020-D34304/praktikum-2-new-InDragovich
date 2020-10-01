f817f32d1f590d29fe999659584e0ae913394877
Agus Indra <agusindra376@gmail.com>
2020-10-01 19:22:57 +0800

    Add files via upload

---
+++ prak2.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_GET['nama'])){
+	$cari = $_GET['nama'];
+	echo "<b>Hasil pencarian : ".$cari."</b>";
+
+	foreach($user as $n){
+	if(strtolower($n["nama"]) == strtolower($Nama)){
+		$a=1;
+		$b=$n;
+	}
+}
+
+
+$user = array(
+	array(  "nama"	        => "Arsene Lupin",
+		"nik"	          => 1234,
+		"jenis_kelamin" => "male",
+		"tgl_lahir"     => "1902-03-23"), 
+	array(  "nama"	        => "Sherlock Holmes", 
+		"nik"			      => 4321,
+		"jenis_kelamin"	=> "male",
+		"tgl_lahir"		  		=> "1876-08-16"), 
+	array(  "nama" 		      	=> "Irene Adler",
+		"nik"			      => 6789,
+		"jenis_kelamin"	=> "female",
+		"tgl_lahir"		  => "1884-10-07"),
+);
+?>
